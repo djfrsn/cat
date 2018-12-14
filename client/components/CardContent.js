f9a337edfdb0931ccecd6e7557f3cbf4dc9c6041
Dennis Jefferson <dennismjefferson@gmail.com>
2018-12-13 21:05:21 -0800

    Favorites UI updated

--- client/components/CardContent.js
+++ client/components/CardContent.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Cat = ({ id, url }) => <img className="cat-image" key={id} src={url} />;
+const Cat = ({ id, url }) => <img className="cat-image" src={url} />;
 
 export const CatCard = ({ cats, is_loading_cats }) => (
   <div className="cats-container">
     {cats.map(({ id, url }) => {
-      return <Cat id={id} url={url} />;
+      return <Cat id={id} url={url} key={id} />;
     })}
     {is_loading_cats && (
       <div className="cats-load-indicator">Loading more cats...</div>
@@ -13,19 +13,31 @@ export const CatCard = ({ cats, is_loading_cats }) => (
   </div>
 );
 
-export const FavoritesCard = ({ favorites }) => {
+export const FavoritesCard = ({ favorites, is_loading_cats }) => {
   return (
     <div className="favorites-container">
       {favorites.length ? (
-        favorites.map(({ id, url }) => {
-          return <Cat id={id} url={url} />;
-        })
+        <CatCard cats={favorites} is_loading_cats={is_loading_cats} />
       ) : (
         <p className="favorites-empty-msg">Favorite a cat!</p>
       )}
     </div>
   );
 };
+
+// export const FavoritesCard = ({ favorites }) => {
+//   return (
+//     <div className="favorites-container">
+//       {favorites.length ? (
+//         favorites.map(({ id, url }) => {
+//           return <Cat id={id} url={url} />;
+//         })
+//       ) : (
+//         <p className="favorites-empty-msg">Favorite a cat!</p>
+//       )}
+//     </div>
+//   );
+// };
 
 export default function cardContent(card_name, props) {
   switch (card_name) {
