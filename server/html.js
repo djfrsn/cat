928d18c9be1e5d632b6610e696020a62035a5b88
Dennis Jefferson <dennismjefferson@gmail.com>
2018-12-15 19:07:49 -0800

    Fix CDN_URL check

--- server/html.js
+++ server/html.js
@@ -1,10 +1,13 @@
 export default function template(title, preloaded_state = {}, app = '') {
+  const CDN_URL =
+    process.env.NODE_ENV === 'production' ? process.env.CDN_URL : '';
+
   let page = `<!DOCTYPE html>
               <html lang="en">
               <head>
                 <meta charset="utf-8">
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
-                <link rel="stylesheet" href="/styles.css">
+                <link rel="stylesheet" href="${CDN_URL}/styles.css">
                 <meta name="viewport" content="width=device-width, initial-scale=1">
                 <title>${title}</title>
               </head>
@@ -13,7 +16,7 @@ export default function template(title, preloaded_state = {}, app = '') {
                   <script>
                     window.__STATE__ = ${JSON.stringify(preloaded_state)}
                   </script>
-                  <script src="/bundle.js"> </script>
+                   <script src="${CDN_URL}/bundle.js"> </script>
               </body>
               </html>
               `;
