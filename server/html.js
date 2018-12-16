43d91080c246f979fd8d687a102c60d5492d03f3
Dennis Jefferson <dennismjefferson@gmail.com>
2018-12-15 18:50:22 -0800

    Deploy progress

--- server/html.js
+++ server/html.js
@@ -4,7 +4,7 @@ export default function template(title, preloaded_state = {}, app = '') {
               <head>
                 <meta charset="utf-8">
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
-                <link rel="stylesheet" href="styles.css">
+                <link rel="stylesheet" href="/styles.css">
                 <meta name="viewport" content="width=device-width, initial-scale=1">
                 <title>${title}</title>
               </head>
@@ -13,7 +13,7 @@ export default function template(title, preloaded_state = {}, app = '') {
                   <script>
                     window.__STATE__ = ${JSON.stringify(preloaded_state)}
                   </script>
-                  <script src="bundle.js"> </script>
+                  <script src="/bundle.js"> </script>
               </body>
               </html>
               `;
